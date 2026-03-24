a7336ee9dc7229cc15e6479441992b1c9108c2fc
Sarvesh Mohite <142711109+CyberSarvesh@users.noreply.github.com>
2026-03-25 00:03:06 +0530

    adding error middleware to the server.js file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import app from "./app.js";
 
-
-
 const PORT = process.env.PORT || 3000;
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
